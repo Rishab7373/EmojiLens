3a91f9129e57a6330c222af5fd3ba0443e0b36cb
Your Name <you@example.com>
2026-06-24 22:11:46 +0530

    "s4etup clean up"

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// injection of react to html 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
